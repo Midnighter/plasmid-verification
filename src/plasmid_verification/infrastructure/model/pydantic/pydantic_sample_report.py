49f769aa537a1fc74d86669d24a5a5bbca7ca29d
Moritz E. Beber <midnighter@posteo.net>
2021-10-27 13:58:22 +0200

    refactor: change field name

--- src/plasmid_verification/infrastructure/model/pydantic/pydantic_sample_report.py
+++ src/plasmid_verification/infrastructure/model/pydantic/pydantic_sample_report.py
@@ -45,6 +45,12 @@ class PydanticSampleReport(PydanticCustomBase):
         title="Median Quality",
         description="The median Phred quality of the sample read.",
     )
+    phred_quality: List[confloat(ge=0.0, le=65.0)] = Field(
+        ...,
+        alias="phredQuality",
+        title="Phred Quality",
+        description="The Phred quality score at each position of the sequence.",
+    )
     trim_start: Optional[conint(ge=0)] = Field(
         None,
         alias="trimStart",
@@ -59,17 +65,11 @@ class PydanticSampleReport(PydanticCustomBase):
         description="The number of nucleotides trimmed at the end of the sequence due "
         "to low Phred quality and before alignment.",
     )
-    phred_quality: List[confloat(ge=0.0, le=65.0)] = Field(
-        ...,
-        alias="phredQuality",
-        title="Phred Quality",
-        description="The Phred quality score at each position of the sequence.",
-    )
-    smoothed_scores: Optional[List[confloat(ge=0.0, le=65.0)]] = Field(
+    trim_scores: Optional[List[confloat(ge=0.0, le=65.0)]] = Field(
         None,
-        alias="smoothedScores",
-        title="Smoothed Scores",
-        description="The smoothed Phred quality scores.",
+        alias="trimScores",
+        title="Trim Scores",
+        description="The scores used for trimming.",
     )
     sequence: str = Field(..., description="The full nucleotide sequence.")
     strand: Optional[StrandDirection] = None
@@ -96,16 +96,16 @@ class PydanticSampleReport(PydanticCustomBase):
             id=report.sample.identifier,
             read_length=len(report.sample),
             median_quality=report.median_quality,
+            phred_quality=list(report.sample.phred_quality),
             trim_start=report.trim_begin,
             trim_end=report.trim_end,
-            phred_quality=list(report.sample.phred_quality),
             sequence=str(report.sample.sequence),
             strand=report.strand,
             errors=report.errors,
             warnings=report.warnings,
         )
         if report.alignment is not None:
-            result.smoothed_scores = list(report.smoothed)
+            result.trim_scores = list(report.smoothed)
         counter = Counter()
         for conflict in report.conflicts:
             result.conflicts.append(PydanticConflictReport.from_domain(conflict))
