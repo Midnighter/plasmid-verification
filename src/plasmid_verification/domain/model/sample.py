844b38047b1bf19fa9903299a1e6470583b9c3d5
Moritz E. Beber <midnighter@posteo.net>
2021-11-20 22:11:58 +0100

    feat: include sample trace data

--- src/plasmid_verification/domain/model/sample.py
+++ src/plasmid_verification/domain/model/sample.py
@@ -18,7 +18,7 @@
 
 from __future__ import annotations
 
-from typing import Iterable
+from typing import Iterable, Dict, Optional
 
 import numpy as np
 from Bio.Seq import Seq
@@ -27,7 +27,14 @@ from Bio.SeqRecord import SeqRecord
 
 class Sample:
     def __init__(
-        self, *, identifier: str, sequence: Seq, phred_quality: Iterable, **kwargs
+        self,
+        *,
+        identifier: str,
+        sequence: Seq,
+        phred_quality: Iterable,
+        peak_positions: Optional[Iterable] = None,
+        traces: Optional[Dict[str, Iterable]] = None,
+        **kwargs,
     ) -> None:
         """"""
         super().__init__(**kwargs)
@@ -38,14 +45,39 @@ class Sample:
             f"The sample's ({identifier}) sequence must have a Phred quality score for "
             f"each position."
         )
+        if peak_positions is not None:
+            self._peak_positions = np.asarray(peak_positions)
+            assert len(self._sequence) == len(self._peak_positions), (
+                f"The sample's ({identifier}) number of peaks must correspond to the "
+                f"sequence length."
+            )
+        else:
+            self._peak_positions = None
+        if traces is not None:
+            self._traces = {key: np.asarray(trace) for key, trace in traces.items()}
+            assert len(self._traces) == 4, "There must be exactly one trace per base."
+            for base, trace in self._traces.items():
+                assert len(trace) >= len(self._peak_positions), (
+                    f"Trace {base} should contain more data points than the number of "
+                    f"peak positions."
+                )
+        else:
+            self._traces = None
 
     @classmethod
     def from_ab1(cls, record: SeqRecord) -> Sample:
         """"""
+        base_order = record.annotations["abif_raw"]["FWO_1"].decode("ascii")
+        analyzed_data_channels = ("DATA9", "DATA10", "DATA11", "DATA12")
         return cls(
             identifier=record.id,
             sequence=record.seq,
             phred_quality=record.letter_annotations["phred_quality"],
+            peak_positions=record.annotations["abif_raw"]["PLOC2"],
+            traces={
+                base: record.annotations["abif_raw"][channel]
+                for base, channel in zip(base_order, analyzed_data_channels)
+            },
         )
 
     def __len__(self) -> int:
